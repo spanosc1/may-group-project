700676b7a81df9a641433d726367fe19332f2040
Jeremy Gross <jagross66@gmail.com>
2016-05-18 16:11:08 -0400

    added shopping list api endpoints

--- controllers/app_controller.js
+++ controllers/app_controller.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var request = require("request");
 var path = require("path");
@@ -16,119 +15,172 @@ var currentRecipeList = [];
 
 // Route for adding (posting) new burger to MYSQL via ORM from input box
 
-module.exports = function(app)
-{
+module.exports = function(app) {
 
-app.get("/", function(req, res){
- 		
-	res.render("index", {recipeSearch: searchOrNot, recipeArray: currentRecipeList});
-	// res.sendFile(path.join(__dirname+"/../views/oldindex.html"));
+    app.get("/", function(req, res) {
 
-})
+        res.render("index", {
+            recipeSearch: searchOrNot,
+            recipeArray: currentRecipeList
+        });
+        // res.sendFile(path.join(__dirname+"/../views/oldindex.html"));
 
-.get("/planner", function(req, res){
+    })
 
-	res.render("planner");
-})
+    .get("/planner", function(req, res) {
 
-.get("/search/:term?", function(req, res) {
-	request('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?mashape-key=WQFSAkYbqamshlMXWe0X3EnVkSmap13txkbjsna2ZL3tOG8BzJ&limitLicense=false&offset=0&number=100&query=' + req.params.term, function(error, response, body)
-	{
-	if(error){
-		res.sendStatus(204);
-	} else {
-		var json = JSON.parse(body);
-		// res.send(json);
+        res.render("planner");
+    })
 
-		// searchorNot:  When the page reloads (at the / endpoint),
-		// handlebars will know to insert results
+    .get("/search/:term?", function(req, res) {
+        request('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?mashape-key=WQFSAkYbqamshlMXWe0X3EnVkSmap13txkbjsna2ZL3tOG8BzJ&limitLicense=false&offset=0&number=100&query=' + req.params.term, function(error, response, body) {
+            if (error) {
+                res.sendStatus(204);
+            } else {
+                var json = JSON.parse(body);
+                // res.send(json);
 
-		searchOrNot = true;
-		currentRecipeList = json.results;
-		res.sendStatus(200);
-	}
-	});
-})
+                // searchorNot:  When the page reloads (at the / endpoint),
+                // handlebars will know to insert results
 
-.get("/ingredientlist/:id?", function(req, res) {
-	request("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" 
-		+req.params.id+"/information?mashape-key="+key, function(error, response, body)
-	{
-	if(error){
-		res.sendStatus(204);
-	} else {
-		var json = JSON.parse(body);
-		 
-		res.send(json.extendedIngredients);
-	}
-	});
-})
+                searchOrNot = true;
+                currentRecipeList = json.results;
+                res.sendStatus(200);
+            }
+        });
+    })
 
-// add a user
+    .get("/ingredientlist/:id?", function(req, res) {
+        request("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + req.params.id + "/information?mashape-key=" + key, function(error, response, body) {
+            if (error) {
+                res.sendStatus(204);
+            } else {
+                var json = JSON.parse(body);
 
-.post("/adduser", function(req, res){
+                res.send(json.extendedIngredients);
+            }
+        });
+    })
 
-	var myname = req.body.myname;
-    var myemail = req.body.myemail;
-    var mybudgetmax = req.body.mybudgetmax;
+    // add a user
 
-     myDB.userinfo.create({
+    .post("/adduser", function(req, res) {
+
+        var myname = req.body.myname;
+        var myemail = req.body.myemail;
+        var mybudgetmax = req.body.mybudgetmax;
+
+        myDB.userinfo.create({
             fullname: myname,
             email: myemail,
             budget: mybudgetmax
         }).then(function() {
-            res.redirect("/");  // ??
+            res.redirect("/"); // ??
         });
-})
+    })
 
-// verify user by email address
+    // verify user by email address
 
-.post("/verifyuser/:email", function(req, res){
-		console.log("request received!");
-		var emailgiven = req.params.email;
+    .post("/verifyuser/:email", function(req, res) {
+        console.log("request received!");
+        var emailgiven = req.params.email;
 
-		myDB.userinfo.findAll({
-			where: {
-				email: emailgiven
-			}
-		}). then(function(usersArray){
-			console.log("usersarray" + usersArray);
-				res.json(usersArray);
-		});
+        myDB.userinfo.findAll({
+            where: {
+                email: emailgiven
+            }
+        }).then(function(usersArray) {
+            console.log("usersarray" + usersArray);
+            res.json(usersArray);
+        });
 
-})
+    })
 
-.post("/storerecipe", function(req, res){
+    // Store recipe in user's 'recipe box'
+    // (Not completed)
+
+    .post("/storerecipe/:userid", function(req, res) {
+
+    	var user = req.params.userid;
+
+        var Recipe = DB.recipebox.create('recipe', {
+            // id: 0,
+
+            // api_id:    
+
+            // recipe_name: 
+
+            // ingredients: 
+
+            // cost: 
+
+            // calories: 
+
+            // userid: 
+        });
+    })
+
+    // Get shopping list for a given user id
+
+    .get("/getshoplist/:userid", function(req, res) {
+
+        //userid will be the person's id in userinfo table
+
+        var user = req.params.userid;
+
+        myDB.shoppinglist.findAll({
+            where: {
+                userid: user
+            }
+        }).then(function(listArray) {
+            console.log("shopping list items: " + listArray);
+            res.json(listArray);
+            // Array of objects will contain
+            // every row in the shoppinglist table
+            // attributable to the user selected.
+            // In other words, their current shopping list.
+            // Each position in the array correponds to one recipe
+            // that was chosen by the user.
+
+        });
 
 
-var Recipe= DB.recipebox.create('recipe',{
-    // id: 0,
+    })
+
+    // Takes form data / data submitted via ajax post
+    // and parses it (as req.body), then puts data into
+    // shopping list table.  Each row in the table is
+    // attributable to one recipe, one user. 
+    //
+    // Above, we get the list by just searching for any items
+    // that are attributable to a user.   
+
+    .post("/addshoplist/:userid", function(req, res){
  
-    // api_id:    
-    	 
-    // recipe_name: 
-       
-    // ingredients: 
-       
-    // cost: 
-       
-    // calories: 
-});
+    	var user = req.params.userid;
 
+    	var recipeid = req.body.recipefor,
+    		recipetitle = req.body.recipetitle,
+    		list = req.body.list,
+    		totalprice = req.body.totalprice;
 
-})
+    	myDB.shoppinglist.create({
+            recipefor: recipeid,    //again, this is the spoontacular
+            recipetitle: recipetitle,	//recipe id number.
+            listtobuy : list,
+            totalprice: totalprice,
+            userid: user
+        }).then(function() {
+            res.redirect("/"); // ??
+        });
 
-.use(function(req, res, next){
+    })
 
-	res.redirect("/");
-	next();
+    .use(function(req, res, next) {
 
-});
+        res.redirect("/");
+        next();
+
+    });
 
 };
-
-
-
-
-
-
